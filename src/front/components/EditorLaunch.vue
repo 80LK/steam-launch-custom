8d54952d8bdf7b2ee9b730a0470bcde9967a2d73
80LK <80lkru@gmail.com>
2024-12-03 01:39:33 +0300

    add validation for adding launch

--- src/front/components/EditorLaunch.vue
+++ src/front/components/EditorLaunch.vue
@@ -3,6 +3,7 @@ import { mdiApplicationOutline, mdiCog, mdiFolder, mdiPencil } from '@mdi/js';
 import { watch, ref, toRaw, computed } from 'vue';
 import ILaunch, { INIT_LAUNCH } from '../../ILaunch';
 import FilePicker from './FilePicker.vue';
+import { SubmitEventPromise } from 'vuetify';
 
 const launch = defineModel<ILaunch | null>({
 	default: null,
@@ -24,7 +25,10 @@ function cancel() {
 	open.value = false;
 	launch.value = null;
 }
-function submit() {
+async function submit(event: SubmitEventPromise) {
+	const result = await event;
+	if (!result.valid) return;
+
 	if (launch.value)
 		Object.assign(launch.value, cacheLaunch.value);
 
@@ -48,36 +52,48 @@ const defaultPathForWorkDir = computed(() => {
 	return l.workdir;
 })
 
+function blockNullRule(thing: string) {
+	return (value: string) => {
+		// console.log(thing, [!value, (value?.length || 0) == 0])
+		if (value.length == 0)
+			return `${thing} must be set`;
+		return true;
+	}
+}
+
 </script>
 
 <template>
 	<v-dialog max-width="600" :model-value="open" persistent @afterLeave="clear" scrollable>
-		<v-card v-if="launch">
-			<v-card-item :prepend-icon="mdiPencil">
-				<v-card-title>
-					{{ edit ? `Edit laucnh "${launch.name}"` : 'New router' }}
-				</v-card-title>
-			</v-card-item>
-			<v-divider />
-			<v-card-text>
-				<v-text-field label="Title" variant="outlined" v-model="cacheLaunch.name" />
+		<v-form @submit.prevent="submit">
+			<v-card v-if="launch">
+				<v-card-item :prepend-icon="mdiPencil">
+					<v-card-title>
+						{{ edit ? `Edit laucnh "${launch.name}"` : 'New router' }}
+					</v-card-title>
+				</v-card-item>
+				<v-divider />
+				<v-card-text>
+					<v-text-field label="Title" variant="outlined" v-model="cacheLaunch.name"
+						:rules="[blockNullRule('Title')]" />
 
-				<FilePicker label="Executable file" variant="outlined" v-model:model-value="cacheLaunch.execute"
-					:icon="mdiApplicationOutline" :file="{ name: 'Application', extensions: ['exe'] }"
-					:default-path="defaultPathForExe" />
+					<FilePicker label="Executable file" variant="outlined" v-model:model-value="cacheLaunch.execute"
+						:icon="mdiApplicationOutline" :file="{ name: 'Application', extensions: ['exe'] }"
+						:default-path="defaultPathForExe" :rules="[blockNullRule('Executable file')]" />
 
-				<v-combobox :prepend-inner-icon="mdiCog" label="Launch options" variant="outlined"
-					v-model="cacheLaunch.launch" clearable chips multiple closable-chips
-					hint="Press enter for add parameter"></v-combobox>
+					<v-combobox :prepend-inner-icon="mdiCog" label="Launch options" variant="outlined"
+						v-model="cacheLaunch.launch" clearable chips multiple closable-chips
+						hint="Press enter for add parameter"></v-combobox>
 
-				<FilePicker label="Work Directory" variant="outlined" v-model:model-value="cacheLaunch.workdir"
-					:icon="mdiFolder" file="directory" :default-path="defaultPathForWorkDir" />
-			</v-card-text>
-			<v-divider />
-			<v-card-actions>
-				<v-btn color="error" @click="cancel()">Cancel</v-btn>
-				<v-btn color="success" @click="submit()">Save</v-btn>
-			</v-card-actions>
-		</v-card>
+					<FilePicker label="Work Directory" variant="outlined" v-model:model-value="cacheLaunch.workdir"
+						:icon="mdiFolder" file="directory" :default-path="defaultPathForWorkDir" />
+				</v-card-text>
+				<v-divider />
+				<v-card-actions>
+					<v-btn color="error" @click="cancel()">Cancel</v-btn>
+					<v-btn color="success" type="submit">Save</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-form>
 	</v-dialog>
 </template>
--- src/front/components/FilePicker.vue
+++ src/front/components/FilePicker.vue
@@ -4,13 +4,15 @@ import { mdiFile } from '@mdi/js';
 import { computed } from 'vue';
 import { VTextField } from 'vuetify/components';
 type IconValue = VTextField['$props']['clearIcon']
+type Rules = VTextField['$props']['rules']
 
 type FilePickerProps = {
 	file?: FileType,
 	icon?: IconValue
 	variant?: "filled" | "underlined" | "outlined" | "plain" | "solo" | "solo-inverted" | "solo-filled" | undefined,
 	label?: string,
-	defaultPath?: string
+	defaultPath?: string,
+	rules?: Rules
 };
 
 const {
@@ -18,11 +20,13 @@ const {
 	icon = mdiFile,
 	variant,
 	label,
-	defaultPath = undefined
+	defaultPath = undefined,
+	rules
 } = defineProps<FilePickerProps>()
 
-const value = defineModel<string>();
+const value = defineModel<string>({ default: '' });
 const isNotEmpty = computed(() => !!(value.value));
+
 
 function selectFile() {
 	App.selectFile(file, defaultPath).then(e => {
@@ -37,15 +41,17 @@ function focus(e: FocusEvent) {
 </script>
 
 <template>
-	<v-input class="v-text-field cursor-pointer" v-slot:default="{ id }">
-		<v-field :variant="variant" :label="label" :id="id.value" @click="selectFile()" :active="isNotEmpty">
+	<v-input class="v-text-field cursor-pointer" v-slot:default="{ id, isValid, isValidating }" :rules="rules"
+		v-model="value">
+		<v-field :variant="variant" :label="label" :id="id.value" @click="selectFile()" :active="isNotEmpty"
+			:error="isValid.value == false">
 			<template v-slot:prepend-inner>
 				<v-icon :icon="icon" class="cursor-pointer" />
 			</template>
 			<input :id="id.value" class="v-field__input cursor-pointer" @focus="focus" v-model="value" />
 
 			<template v-slot:append-inner>
-				<v-icon icon="$clear" v-if="isNotEmpty" @click.stop="value = undefined" />
+				<v-icon icon="$clear" v-if="isNotEmpty" @click.stop="value = ''" />
 			</template>
 		</v-field>
 	</v-input>
